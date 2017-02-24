90e8223ec75db673724121429b8c1b61d62f2e0a
Idyllic <nikamanish007@gmail.com>
2017-02-24 16:00:28 +0530

    Initial Comit

---
+++ app.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var express  = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.set("views",path.resolve(__dirname,"views"));
+app.set("view engine","ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+var orders = [];
+app.locals.orders = orders;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(request,response){
+	response.render("index");
+});
+
+app.get('/new-order', function(request, response){
+	response.render("add_order")
+});
+
+app.post('/new-order', function(request, response){
+	if(!request.body.table_no || !request.body.item_list){
+		response.status(400).send("Both fields must be present");
+		return;
+	}
+	orders.push({
+		"table_no" : request.body.table_no,
+		"item_list":request.body.item_list,
+		"ordered_at": new Date()
+	});
+	response.redirect("/");
+});
+
+http.createServer(app).listen(process.env.PORT ||4000, function(){
+	console.log("Server: 4000");
+})
